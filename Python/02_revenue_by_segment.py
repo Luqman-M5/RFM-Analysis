190c445e36a7dfb37426597d619e084bf528c665
Luqman-M5 <l.muhammedkunju@se24.qmul.ac.uk>
2026-02-16 20:07:14 +0000

    Added the code of 02 file revenue by each segment shown in what segment owns what percentage of the total revenue

--- Python/02_revenue_by_segment.py
+++ Python/02_revenue_by_segment.py
@@ -4,3 +4,25 @@ import numpy as np
 
 rfm = pd.read_csv(r"Python\rfmdata.csv")
 
+revenue_by_segment = rfm.groupby('segment', as_index=False)['monetary'].sum()
+revenue_by_segment.rename(columns={'monetary': 'Total_Revenue'}, inplace=True)
+revenue_by_segment_percentage = revenue_by_segment['Total_Revenue'] / revenue_by_segment['Total_Revenue'].sum() * 100
+
+plt.figure(figsize=(8,4))
+bars = plt.bar(revenue_by_segment['segment'], revenue_by_segment['Total_Revenue'], color='lightblue', edgecolor='black')
+for bar,pct in zip(bars, revenue_by_segment_percentage):
+    plt.text(
+        bar.get_x() + bar.get_width() / 2,
+        bar.get_height(),
+        f'{pct:.1f}%',
+        ha='center',
+        va='bottom',
+        fontsize=8
+    )
+
+plt.title('Total Revenue by Customer Segment')
+plt.xlabel('Customer Segment')
+plt.ylabel('Total Revenue')
+plt.xticks(rotation=45, ha='right')
+plt.tight_layout()
+plt.show()
