85d217a5758bf7063a8174c15e222de5f2e231a6
Luqman-M5 <l.muhammedkunju@se24.qmul.ac.uk>
2026-02-16 23:48:10 +0000

    Added the code to 03 file py for the avg value of customer by segment represented by using scatterplot

--- Python/03_avg_value_per_segment.py
+++ Python/03_avg_value_per_segment.py
@@ -3,3 +3,15 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 rfm = pd.read_csv(r"Python\rfmdata.csv")
+
+avg_value_per_segment = rfm.groupby('segment', as_index=False)['monetary'].mean()
+avg_value_per_segment.rename(columns={'monetary': 'Average_Monetary'}, inplace=True)
+
+plt.figure(figsize=(8,4))
+plt.scatter(avg_value_per_segment['segment'], avg_value_per_segment['Average_Monetary'], color='lightblue')
+plt.title('Average Monetary Value by Customer Segment')
+plt.xlabel('Customer Segment')
+plt.ylabel('Average Monetary Value')
+plt.xticks(rotation=45, ha='right')
+plt.tight_layout()
+plt.show()
--- Python/04_recency_vs_spend.py
+++ Python/04_recency_vs_spend.py
@@ -3,3 +3,5 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 rfm = pd.read_csv(r"Python\rfmdata.csv")
+
+
